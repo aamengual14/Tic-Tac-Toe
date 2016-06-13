b854c0cccbf8732c9199b18464cbf2644b003132
Alfredo Amengual <amengual.alfredo@gmail.com>
2016-06-12 23:50:09 -0500

    math commented out, getWinner not functioning

--- script.js
+++ script.js
@@ -1,18 +1,10 @@
-//Establish a player 1 and a player 2
-//When player 1 clicks, the button changes to an X
-//When player 2 clicks, the button changes to o
-//there needs to be a switching mechanism from player1 to player 2
-//establish all of the winning combinations
-//if the player1 has any of the winning combos, he wins
-//if the player2 has any of the winning combos, he wins the game
-//math funciton when you click, if naswer right, you receiver the square
-//if not other player receives squar
-$(document).ready(function() {
+ $(document).ready(function() {
 
-  var playerX = "X";
-  var playerO = "O";
 
-  var math = {
+ var state = false;
+ var currentPlayer = 1;
+
+ var math = {
     one: "8 * 5",
     two: "6 * 7",
     three: "9 * 8",
@@ -26,23 +18,58 @@ $(document).ready(function() {
   };
 
 
+ var askRandomEquation = function() {
+   var result;
+   var count = 0;
+   for (var key in math) {
+     if (math.hasOwnProperty(key)) {
+       if (Math.random() < 1/++count) {
+         result = math[key];
+       }
+     }
+  }
+  return result;
+}
 
 
 
-$('button').click(function(event) {
-    var result;
-    var count = 0;
-    for (var key in math)
-      if (math.hasOwnProperty(key)) {
-        if (Math.random() < 1/++count) {
-          result = math[key];
-        }
-      }
-    var test = eval(result);
-    var answer = eval(prompt(result));
-    if (test === answer) {
-      alert("good");
+function switchPlayer() {
+    if (currentPlayer === 1){
+        currentPlayer = 2;
+    } else {
+      currentPlayer = 1
+    };
+};
+
+
+function getSquare(number) {
+  return $("s" + number).text;
+}
+
+function checkRow(a, b, c, move) {
+  var result = false;
+  if (getSquare(a) === move && getSquare(b) === move && getSquare(c) === move) {
+    result = true;
+  }
+  return result;
+}
+
+function checkWinner(move) {
+  var result = false;
+  if (move.text != " ") {
+    if (checkRow(1, 2, 3, move) ||
+        checkRow(4, 5, 6, move) ||
+        checkRow(7, 8, 9, move) ||
+        checkRow(1, 4, 7, move) ||
+        checkRow(2, 5, 8, move) ||
+        checkRow(3, 6, 9, move) ||
+        checkRow(1, 5, 9, move) ||
+        checkRow(3, 5, 7, move)) {
+      result = true;
     }
+  }
+  return alert('winner');
+}
 
 
 
@@ -50,6 +77,29 @@ $('button').click(function(event) {
 
 
 
+
+
+   $('button').one('click', function(event) {
+
+  //var equation = askRandomEquation();
+   state = !state;
+   if (state) {
+     //equation;
+     //var test = eval(equation);
+     //var answer = eval(prompt(equation));
+     //if (answer === test) {
+      $(this).html('X');
+      checkWinner('X');
+      switchPlayer();
+     } else {
+      //alert('wrong, opponent steals square.');
+      $(this).html('O');
+      checkWinner('0');
+      //switchPlayer();
+      }
+
+
+});
 });
 
 
@@ -57,4 +107,57 @@ $('button').click(function(event) {
 
 
 
+/*
+    $('button').one('click', function(event) {
+
+       var equation = askRandomEquation();
+   state = !state;
+   if (state) {
+     equation;
+     var test = eval(equation);
+     var answer = eval(prompt(equation));
+     if (answer === test) {
+      $(this).html('X');
+      switchPlayer();
+     } else {
+      alert('wrong');
+      equation;
+        var test = eval(equation);
+        var answer = eval(prompt(equation));
+         if (answer === test) {
+           $(this).html('O');
+         } else {
+          alert('wrong');
+          switchPlayer();
+         }
+     }
+   }
+  });
+
+  */
+
+  /*$('button').one('click', function(event) {
+
+  //var equation = askRandomEquation();
+   state = !state;
+   if (state) {
+     //equation;
+     //var test = eval(equation);
+     //var answer = eval(prompt(equation));
+     //if (answer === test) {
+      $(this).html('X');
+      switchPlayer();
+     } else {
+      //alert('wrong, opponent steals square.');
+      $(this).html('O');
+      //switchPlayer();
+      }
+
+
 });
+
+*/
+
+
+
+
